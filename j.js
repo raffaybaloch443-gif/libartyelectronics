22e2dd9927cd89a28f5836cdd1798e5d73c3b1f2
raffay-baloch443-gif <raffaybaloch443@gmail.com>
2026-02-02 06:49:01 -0800

    git hub adding

---
+++ j.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+        $(".cart").click(function(){
+            window.location.href = "cart.html";
+        });
+
+        $(".like").click(function(){
+            window.location.href = "likes.html";
+        });
+
+        $(".login-form").click(function(){
+            window.location.href = "login-form.html";
+        });
+    });
